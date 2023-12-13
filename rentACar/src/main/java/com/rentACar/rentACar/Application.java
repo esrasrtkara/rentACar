836cdbd9dfa406415cf9bd65da86c6800bbd641d
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-12-13 21:38:39 +0300

    model mapper

--- rentACar/src/main/java/com/rentACar/rentACar/Application.java
+++ rentACar/src/main/java/com/rentACar/rentACar/Application.java
@@ -1,7 +1,9 @@
 package com.rentACar.rentACar;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,5 +12,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-
+	@Bean
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
+	}
 }
---
+++ rentACar/src/main/java/com/rentACar/rentACar/core/utilities/mappers/ModelMapperService.java
@@ -0,0 +1,8 @@
+package com.rentACar.rentACar.core.utilities.mappers;
+
+import org.modelmapper.ModelMapper;
+
+public interface ModelMapperService {
+    ModelMapper forResponse();
+    ModelMapper forRequest();
+}
