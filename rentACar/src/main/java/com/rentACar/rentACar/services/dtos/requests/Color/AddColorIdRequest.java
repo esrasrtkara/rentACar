9b05ee462049edfb872f19842110f132ffde3107
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-12-14 18:57:20 +0300

    Add request

---
+++ rentACar/src/main/java/com/rentACar/rentACar/services/dtos/requests/Color/AddColorIdRequest.java
@@ -0,0 +1,9 @@
+package com.rentACar.rentACar.services.dtos.requests.Color;
+
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+
+@Data
+public class AddColorIdRequest {
+    private int id;
+}
