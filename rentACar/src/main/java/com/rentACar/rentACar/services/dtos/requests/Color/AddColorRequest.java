392b8dd8fc55cccfc0da7f28a8b1ad1acfd24e3f
Abdulkadir Aydemir <aydemir_abdulkadir@outlook.com>
2023-12-15 14:18:08 +0300

    color request ler eklendi

--- rentACar/src/main/java/com/rentACar/rentACar/services/dtos/requests/Color/AddColorRequest.java
+++ rentACar/src/main/java/com/rentACar/rentACar/services/dtos/requests/Color/AddColorRequest.java
@@ -1,10 +1,11 @@
 package com.rentACar.rentACar.services.dtos.requests.Color;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
+@Data
 
 public class AddColorRequest {
+    @Size(min = 2 , message = "Renk adı 2 karakterden küçük olamaz!")
+    private String name;
 }
