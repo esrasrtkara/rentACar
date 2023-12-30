6e8a637f8193cc1c703aa621aee567f17aa64d5d
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-12-30 14:29:51 +0300

    update Color Entity

--- rentACar/src/main/java/com/rentACar/rentACar/entities/concretes/Color.java
+++ rentACar/src/main/java/com/rentACar/rentACar/entities/concretes/Color.java
@@ -1,6 +1,7 @@
 package com.rentACar.rentACar.entities.concretes;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rentACar.rentACar.entities.abstracts.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,16 +14,13 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Color {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
+public class Color extends BaseEntity {
 
     @Column(name = "name")
     private String name;
+    @Column(name = "code")
+    private String code;
 
     @OneToMany(mappedBy = "color")
-    @JsonIgnore
     private List<Car> cars;
 }
