88a223f3458238c7881a4206df20d136d6de0d59
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2024-02-16 17:38:02 +0300

    add httpStatus  modelsController and brandsControllers

--- rentACar/src/main/java/com/rentACar/rentACar/services/dtos/requests/Model/AddModelRequest.java
+++ rentACar/src/main/java/com/rentACar/rentACar/services/dtos/requests/Model/AddModelRequest.java
@@ -1,5 +1,7 @@
 package com.rentACar.rentACar.services.dtos.requests.Model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -7,6 +9,9 @@ import lombok.Data;
 
 public class AddModelRequest {
     @Size(min = 2, message = "Model name cannot be less than 2 characters")
+    @NotNull(message = "Model name cannot be null.")
     private String name;
+    @NotNull(message = "Brand id cannot be null.")
+    @Positive(message = "Id must be a positive number.")
     private int brandId;
 }
