229e187bec4ce19e1ad0a935dc966ec90ea2c341
Abdulkadir Aydemir <aydemir_0225@hotmail.com>
2024-02-19 16:17:44 +0300

    corporateCustomer update

--- rentACar/src/main/java/com/rentACar/rentACar/repositories/CorporateCustomerRepository.java
+++ rentACar/src/main/java/com/rentACar/rentACar/repositories/CorporateCustomerRepository.java
@@ -4,4 +4,5 @@ import com.rentACar.rentACar.entities.concretes.CorporateCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CorporateCustomerRepository  extends JpaRepository<CorporateCustomer,Integer> {
+    CorporateCustomer findByUserId(int userId);
 }
